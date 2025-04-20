0138fd94397838c7fcc02206d33e0fa8b365b8af
kutta97 <yhjin04170@gmail.com>
2025-04-21 01:35:21 +0900

    feat: useProduct 훅 로직을 작성한다

--- src/refactoring/hooks/useProduct.ts
+++ src/refactoring/hooks/useProduct.ts
@@ -2,9 +2,25 @@ import { useState } from "react";
 import { Product } from "../../types.ts";
 
 export const useProducts = (initialProducts: Product[]) => {
+  const [products, setProducts] = useState<Product[]>(() => initialProducts);
+
+  const updateProduct = (updatedProduct: Product) => {
+    setProducts((products) =>
+      products.map((product) =>
+        product.id === updatedProduct.id
+          ? { ...product, ...updatedProduct }
+          : product,
+      ),
+    );
+  };
+
+  const addProduct = (product: Product) => {
+    setProducts((products) => [...products, product]);
+  };
+
   return {
-    products: [],
-    updateProduct: () => undefined,
-    addProduct: () => undefined,
+    products,
+    updateProduct,
+    addProduct,
   };
 };
