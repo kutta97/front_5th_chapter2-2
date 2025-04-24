99b9788f88f0f4eb329c4c4119ad4b9e3a629271
kutta97 <yhjin04170@gmail.com>
2025-04-25 06:49:16 +0900

    chore: baseUrl 빌드 환경 설정 추가

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,27 @@
 import { defineConfig as defineTestConfig, mergeConfig } from 'vitest/config';
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react-swc';
+import { resolve } from 'path';
 
-export default mergeConfig(
-  defineConfig({
-    plugins: [react()],
-  }),
-  defineTestConfig({
-    test: {
-      globals: true,
-      environment: 'jsdom',
-      setupFiles: './src/setupTests.ts'
-    },
-  })
-)
+export default ({mode}: { mode: string }) => {
+  const env = loadEnv(mode, process.cwd(), "");
+
+  return mergeConfig(
+    defineConfig({
+      plugins: [react()],
+      base: env.VITE_BASE_URL,
+      build: {
+        rollupOptions: {
+          input: resolve(__dirname, 'index.refactoring.html'),
+        },
+      },
+    }),
+    defineTestConfig({
+      test: {
+        globals: true,
+        environment: 'jsdom',
+        setupFiles: './src/setupTests.ts'
+      },
+    })
+  )
+}
